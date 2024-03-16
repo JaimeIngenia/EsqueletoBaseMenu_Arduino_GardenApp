50897823a5aadb84b417912d1229bf88ec9f7863
JaimeIngenia <jamoncayop@unal.edu.co>
2024-03-16 04:45:50 -0500

    limpieza

--- Actualizaciones_2024/23_2_base_encoder/23_2_base_encoder.ino
+++ Actualizaciones_2024/23_2_base_encoder/23_2_base_encoder.ino
@@ -1,86 +1,36 @@
 #define CLK 2
-
 #define DT 3
-
 #define SW 5
 
  
-
 int counter = 0;
-
 int currentStateCLK;
-
 int lastStateCLK;
-
 String currentDir = "";
-
 unsigned long lastButtonPress = 0;
-
-unsigned long debounceDelay = 50; // Tiempo de espera anti-rebote en milisegundos
-bool buttonPressed = false;
-
 int estado=1;
 int estadoAnterior=1;
  
 
 void setup() {
-
-  
-
-        //configurar pines de encoder como entradas
-
         pinMode(CLK,INPUT);
-
         pinMode(DT,INPUT);
-
         pinMode(SW, INPUT_PULLUP);
-
-        
-
-        // Configurar Monitor Serial 
-
         Serial.begin(9600);
-
-        
-
-        // leer el estado inicial de CLK 
-
         lastStateCLK = digitalRead(CLK);
-
 }
 
  
 
 void loop() {
 
-    // estado = digitalRead(SW);
-
-    // // Debounce
-    // if (estado != estadoAnterior) {
-    //   delay(60);  
-    //   estado = digitalRead(SW);
-    // }
-
-    // if((estado == LOW)&&(estadoAnterior == HIGH))
-    //     {
-    //         Serial.println("pRESIONADOOOO");
-
-    //         estadoAnterior = estado;
-    //     }
-
-    // Leer el siguiente estado de CLK
-
     currentStateCLK = digitalRead(CLK);
-
-    
 
     // Si el último y el actual estado de CLK son diferentes son diferentes, entonces ocurrió un pulso
 
     // Reaccionar a solo 1 cambio de estado para evitar conteo doble. 
 
     if (currentStateCLK != lastStateCLK  && currentStateCLK == 1){
-
-    
 
         // Si el estado de DT es diferente que el estado de CLK entonces
 
@@ -136,7 +86,6 @@ void loop() {
         //Si pasaron 50ms desde el ultimo pulso bajo, significa
 
         //que el boton ha sido presionado, liberado y presionado nuevamente 
-        //Serial.println("Button pressed!");
 
         if (millis() - lastButtonPress > 50) {
 
